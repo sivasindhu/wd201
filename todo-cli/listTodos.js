7fa8d4f8fa528088f8afab93245ece9b6b1deee3
sivasindhu <116018472+sivasindhu@users.noreply.github.com>
2023-09-13 19:54:07 +0530

    Create listTodos.js

---
+++ todo-cli/listTodos.js
@@ -0,0 +1,15 @@
+//  listTodos.js
+const db = require("./models/index");
+
+
+const listTodo = async () => {
+  try {
+
+    await db.Todo.showList();
+  } catch (error) {
+    console.error(error);
+  }
+};
+(async () => {
+  await listTodo();
+})();
